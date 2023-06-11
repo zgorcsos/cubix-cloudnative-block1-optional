18f27d7535c596c25d937e25a4a29c505dbaa5f8
Csendes Dávid <csendes.david@alerant.hu>
2023-06-11 16:45:26 +0200

    Initial application

---
+++ README.md
@@ -0,0 +1,10 @@
+# First optional assignment
+
+You can run the application with these commands:
+
+```
+./mvnw clean package
+java -jar target/cubix-first-optional-0.0.1-SNAPSHOT.jar
+```
+
+You can test the application with the Postman collection: First Optional Assignment.postman_collection.json
---
+++ src/main/java/hu/cubix/cloud/FirstOptional.java
@@ -0,0 +1,13 @@
+package hu.cubix.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstOptional {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstOptional.class, args);
+	}
+
+}
