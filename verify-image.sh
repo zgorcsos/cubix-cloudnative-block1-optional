96f5a79988d9db427bb7a6f1d768ca9925036a5f
Görcsös Zoltán <gorcsos.zoltan@gmail.com>
2025-07-06 18:06:42 +0200

    Merge pull request #1 from zgorcsos/ci/github_action_workflows
    Merge Ci/GitHub action workflows

---
+++ verify-image.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+docker run -d --name test -p 8080:8080 ghcr.io/zgorcsos/cubix/1/optional-homework/app:springboot > /dev/null
+sleep 10
+curl --fail http://localhost:8080/demo/message
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "Verification failed, here are the logs"
+  docker logs test
+fi
+docker stop test > /dev/null
+docker rm test > /dev/null
+exit $RESULT
